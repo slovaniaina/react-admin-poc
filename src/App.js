b95c1416f302d500b6086b448937743a75498d4a
Lovaniaina Samimamy <lovaniaina.samimamyandrianarisoa@cpexterne.org>
2019-05-16 16:21:07 +0200

    Add new components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource, EditGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './components/users';
+import { PostList, PostEdit, PostCreate } from './components/posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+          <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
+          <Resource name="users" list={UserList} />
+     </Admin>
   );
-}
-
 export default App;
